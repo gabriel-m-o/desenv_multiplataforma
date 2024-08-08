529f23cc9fba537e97475403266161114ecc5d2f
gabriel-m-o <111623684+gabriel-m-o@users.noreply.github.com>
2024-08-08 09:14:04 -0300

    exs aula 1

---
+++ exs.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+//ex1 calcular area de um circulo
+  // void calculaArea(List<String> args) {
+  //   int raio = 10;
+  //   double area = pi * pow(raio, 2);
+  //   print(area);
+  // }
+
+//ex2 encontrar raizes de uma equacao quadratica
+// void main(List<String> args) {
+//   int a = 1, b = -3, c = 2;
+//   num delta = pow(b, 2) - (4 * a * c);
+//   num x1 = (b + delta) / 2 * a;
+//   num x2 = (b - delta) / 2 * a;
+//   print ("[$x1, $x2]");
+// }
+
+//ex3 calcular volume de uma esfera utilizando o raio
+// void main(List<String> args) {
+//   int raio = 4;
+//   num volume = (4/3) * pi * pow(raio, 3);
+//   print(volume);
+// }
+
+//ex4 encontrar o valor da expressao 2 + 3 * 4 - (2 * 3) + 2^3
+// void main(List<String> args) {
+//   num valor = 2 + 3*4 - (2 * 3) + pow(2, 3);
+//   print(valor);
+// }
+
+//ex5 converter temperatura de fahreinheit para celsius
+// double calculaCelsius(double f) {
+//   double C = (f - 32) * (5/9);
+//   return C;
+// }
+// void main(List<String> args) {
+//   double celsius = calculaCelsius(100);
+//   print(celsius);  
+// }
+
+//
