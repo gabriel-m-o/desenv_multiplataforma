0ef53ab6fcd4735866ea3077b42a15c9a293aa9b
gabriel-m-o <111623684+gabriel-m-o@users.noreply.github.com>
2024-08-30 12:56:50 -0300

    flutter inicio

---
+++ colecoes/aprendizado/android/app/src/main/kotlin/com/example/aprendizado/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aprendizado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ colecoes/aprendizado/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main() {
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+      title: const Text('minhas imagens'), 
+      ),
+    )
+  );
+}
